202c49d0192e11568740c9e2c01d2ec3acd3a80f
顾霖轩 <150428815+2495802072@users.noreply.github.com>
2023-11-30 16:35:11 +0800

    Add files via upload

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -3,16 +3,64 @@ const Home = {
     template:`
     <div class="box_right">
         <div class="box_border">
-            <div class="col-8">
+            <div class="home_block">
                 <!-- 格言 -->
+                <p class="Awords"><b>叫醒你的是闹钟,还是梦想？</b></p>
+                <p style="text-align: right;color: #fff">——每日赠言</p>
+                <hr style="color:#fff;margin-bottom: 0;">
                 <!-- 更新日志 -->
-            </div>
-            <div class="col-4">
-                <!-- 超链接 -->
+                <div class="updatas col-12 col-md-8">
+                <p style="background-color: #161616;color: #fff;margin: 0;line-height: 40px;font-size: 22px;"><b>更新日志</b></p>
+                <ul>
+                    <li v-for="item in logs">
+                    <table style="width: 100%;">
+                        <thead style="background-color: #666">
+                            <tr><th>{{item.data}}</th></tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="logs in item.imformation"><th style="color:#ebebeb;padding-left: 20px;">·&nbsp;{{logs}}</th></tr>
+                            
+                        </tbody>
+                    </table>
+                    </li>
+                </ul>
+                </div>
+                <!-- 页面反馈 -->
+                <div class="feedback-operator col-md-4">
+                    <div class="questions"><p style="text-align: center;">我的个人网页可还美观？</p></div>
+                    <div class="answers">
+                        <button class="yes">尚可入眼<br>（num）</button>
+                        <button class="no">完全不行<br>（num）</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    `
+    `,
+    data() {
+        return {
+            logs: [{
+                data: "2023.11.30",
+                imformation: ["编写“首页”结构","添加“每日赠言”,每天随机一条，JS方法待完善","添加“日志内容”记录历程","添加反馈窗,记录访客情况，长期保存数据方法待查，JS方法待完善"]
+                },{
+                data: "2023.11.29",
+                imformation: ["更新“音乐”界面呈现方式","按钮排布在下方时手机端呈现级差，且浏览器比例较扁时按不到","改成和唱碟登高且取消Play按钮，将方法安排在唱碟上","添加唱碟在音乐播放时的动画效果"]
+                },{
+                data: "2023.11.28",
+                imformation: ["“音乐”界面按钮功能实现：上一首、下一首、Play/Pause","给遥控窗添加按钮效果：关闭背景和声音的功能","设置遥控窗手机端隐藏，实在太挡屏幕、占镜头"]
+                },{
+                data: "2023.11.27",
+                imformation: ["版权声明孤零零太单调，添加godot游戏项目选项","调整颜色强调","游戏项目采用超链接页面跳转","项目页有点多余，但暂不做考虑"]
+                },{
+                data: "2023.11.26",
+                imformation: ["编辑背景","使用vedio视频背景","设置手机端和电脑端两个背景，手机端考虑到性能不加视频","调整css样式让视频铺满背景","添加左侧遥控窗并初定尺寸和颜色","遥控窗分区并缩小上半区，使用绝对定位把头像突出去","按钮部分分为两块，页面设置和版权声明"]
+                },{
+                data: "2023.11.25",
+                imformation: ["梦开始的地方，创建工程文件","架构top-nav(顶部导航)","采用bootstrap实现手机端收起到按钮","采用Vue的组件替换实现三个(暂时先三个)页面的跳转","导入化名的LOGO","调整配色"]
+                }
+            ]
+        }
+    }
 }
 
 const Music = {
@@ -91,12 +139,8 @@ const Resume = {
     template:`
     <div class="box_right">
         <div class="box_border">
-            <div class="col-8">1
-                <!-- 格言 -->
-                <!-- 更新日志 -->
-            </div>
-            <div class="col-4">2
-                <!-- 超链接 -->
+            <div class="col-12" style="color:#fff;font-weight:bold;display:flex;">
+                <p style="margin:auto;font-size:30px">此页暂不做更新</p>
             </div>
         </div>
     </div>
